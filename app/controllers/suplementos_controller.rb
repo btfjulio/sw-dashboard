d1afbbcd0af0b4b826873e4edab4a51c3c262ab0
Bruno Tostes <btfjulio@hotmail.com>
2019-10-15 11:38:59 -0300

    change DB to api source

--- app/controllers/suplementos_controller.rb
+++ app/controllers/suplementos_controller.rb
@@ -1,13 +1,14 @@
+require 'open-uri'
 
 class SuplementosController < ApplicationController
     skip_before_action :authenticate_user!, only: %i[index]
-  
-
     def index
+        url = "https://savewhey-api.herokuapp.com/api/v1/stores/1/suplementos?user_email=btfjulio@hotmail.com&user_token=x6nZcxz9jyaR68y3GEsy"
         if params[:query].present?
-            @suplementos = Suplemento.order(:price_cents).sup_search(params[:query]).page(params[:page]).per(50)
-        else
-            @suplementos = Suplemento.all.page(params[:page]).per(50)
+            search =  params[:query].split(' ').join('+')
+            url = url + "&query=#{search}&commit=Search"
         end
+        suple_serialized = open(url).read
+        @suplementos = JSON.parse(suple_serialized, {:symbolize_names => true})
     end
 end
