51d04450be4f8c858a452281c3a298db597709d6
Bruno Tostes <btfjulio@hotmail.com>
2019-10-15 19:00:00 -0300

    add pagination to front end

--- app/controllers/suplementos_controller.rb
+++ app/controllers/suplementos_controller.rb
@@ -3,12 +3,16 @@ require 'open-uri'
 class SuplementosController < ApplicationController
     skip_before_action :authenticate_user!, only: %i[index]
     def index
-        url = "https://savewhey-api.herokuapp.com/api/v1/stores/1/suplementos?user_email=btfjulio@hotmail.com&user_token=x6nZcxz9jyaR68y3GEsy"
+        url = "https://savewhey-api.herokuapp.com/api/v1/suplementos?user_email=btfjulio@hotmail.com&user_token=x6nZcxz9jyaR68y3GEsy"
         if params[:query].present?
             search =  params[:query].split(' ').join('+')
             url = url + "&query=#{search}&commit=Search"
         end
+        if params[:page].present?
+            url = url + "&page=#{params[:page]}"
+        end
         suple_serialized = open(url).read
-        @suplementos = JSON.parse(suple_serialized, {:symbolize_names => true})
+        @headers = JSON.parse(suple_serialized, {:symbolize_names => true})[0]
+        @suplementos = JSON.parse(suple_serialized, {:symbolize_names => true})[1..]
     end
 end
