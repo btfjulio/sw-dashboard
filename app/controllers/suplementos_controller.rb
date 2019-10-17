a3be688066a113be47dd98bcc4f8a18a738c9e4b
Bruno Tostes <btfjulio@hotmail.com>
2019-10-17 16:26:50 -0300

    add price change filter

--- app/controllers/suplementos_controller.rb
+++ app/controllers/suplementos_controller.rb
@@ -6,10 +6,13 @@ class SuplementosController < ApplicationController
         url = "https://savewhey-api.herokuapp.com/api/v1/suplementos?user_email=btfjulio@hotmail.com&user_token=x6nZcxz9jyaR68y3GEsy"
         if params[:query].present?
             search =  params[:query].split(' ').join('+')
-            url = url + "&query=#{search}&commit=Search"
+            url = url + "&query=#{search}"
         end
         if params[:page].present?
             url = url + "&page=#{params[:page]}"
+        end
+        if params[:changed].present?
+            url = url + "&changed=#{params[:changed]}"
         end
         suple_serialized = open(url).read
         @headers = JSON.parse(suple_serialized, {:symbolize_names => true})[0]
