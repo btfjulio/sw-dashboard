168d70ef968cad45827122f5ad5e12d8a61481c9
Bruno Tostes <btfjulio@hotmail.com>
2019-11-19 16:49:02 -0300

    add copy button to bitlinks

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,6 @@
 import "bootstrap";
 import { Clipboard } from 'clipboard';
-
+console.log('<oi></oi>')
 var btns = document.querySelectorAll('.clipboard-btn');
 btns.forEach(function(btn){
     new Clipboard(btn); 
