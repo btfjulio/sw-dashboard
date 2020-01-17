a64a7f8155695e90caafd77cd3a96b4ac9495eda
Bruno Tostes <btfjulio@hotmail.com>
2020-01-17 11:53:05 -0300

    change filter engine

--- app/controllers/suplementos_controller.rb
+++ app/controllers/suplementos_controller.rb
@@ -5,32 +5,9 @@ class SuplementosController < ApplicationController
     skip_before_action :authenticate_user!, only: %i[index get_bitlink]
     def index
         url = "https://savewhey-api.herokuapp.com/api/v1/suplementos?user_email=btfjulio@hotmail.com&user_token=x6nZcxz9jyaR68y3GEsy"
-        if params[:average].present?
-            search =  params[:average].split(' ').join('+')
-            url = url + "&average=#{search}"
-        end
-        if params[:promo].present?
-            search =  params[:promo].split(' ').join('+')
-            url = url + "&promo=#{search}"
-        end
-        if params[:seller].present?
-            search =  params[:seller].split(' ').join('+')
-            url = url + "&seller=#{search}"
-        end
-        if params[:name].present?
-            search =  params[:name].split(' ').join('+')
-            url = url + "&name=#{search}"
-        end
-        if params[:store].present?
-            search =  params[:store].split(' ').join('+')
-            url = url + "&store=#{search}"
-        end
-        if params[:page].present?
-            url = url + "&page=#{params[:page]}"
-        end
-        url = url + "&changed=#{params[:changed]}" if params[:changed].present?
-        url = url + "&supershipping=#{params[:supershipping]}" if params[:supershipping].present?
+        params.each { |key, value| url = url + "&#{key}=#{value}" unless key == 'controller' || key == 'action' }
         suple_serialized = open(url).read
+        @stores = get_stores()
         @headers = JSON.parse(suple_serialized, {:symbolize_names => true})[0]
         @suplementos = JSON.parse(suple_serialized, {:symbolize_names => true})[1..]
     end
@@ -44,6 +21,12 @@ class SuplementosController < ApplicationController
         else
             @shorten_url = Bitly.client.shorten(@suplemento[:link]).short_url
         end
+    end
+
+    def get_stores
+        api_endpoint = "https://savewhey-api.herokuapp.com/api/v1/stores?user_email=btfjulio@hotmail.com&user_token=x6nZcxz9jyaR68y3GEsy"
+        stores_serialized = open(api_endpoint).read
+        JSON.parse(stores_serialized, {:symbolize_names => true})[0]
     end
 
 end
